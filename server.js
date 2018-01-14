5fdd97102e11ea8c61ef9b8ed7d2f0282f1158c6
user <user@ELPBLR-078.evolvus.com>
2018-01-14 17:21:12 +0530

    Initial git

---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname +'/public'));
+
+/*register middle ware*/
+app.use((req,res,next) =>{
+
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log',log+ '\n',(err) =>{
+    if(err){
+      console.log("error while writing the log");
+    }
+  });
+  next();
+});
+
+
+/*manintance handler*/
+app.use((req,res,next) =>{
+  res.render('manintance.hbs');
+});
+
+
+/*to make the maintance handler hands for while rendering static pages also */
+//app.use(express.static(__dirname +'/public'));
+
+
+hbs.registerHelper('getCurrentYear',() =>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text) => {
+  return text.toUpperCase();
+})
+
+app.get('/',(req,res) => {
+
+  //res.send('<h1>Hello express!</h1>');
+  /*res.send({
+    name: 'Manoj',
+    list: ['bikes','cars']
+  });*/
+
+  res.render('home.hbs',{
+    pageTitle: 'Home page',
+    welcomePage: 'hi welcome to my website'
+    //currentYear: new Date().getFullYear()
+
+  })
+});
+
+
+app.get('/about',(req,res) => {
+
+
+  //res.send('<h1>About Page</h1>');
+
+  res.render('about.hbs',{
+    pageTitle: 'About page'
+    //currentYear: new Date().getFullYear()
+
+  })
+});
+
+app.get('/bad',(req,res) => {
+
+  //res.send('Hello express!</h1>');
+  res.send({
+    status:'404',
+    Message:'Not found'
+
+  });
+});
+
+app.listen(3000);
